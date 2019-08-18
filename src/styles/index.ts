fd06ce21ada574f01a647b0a82b39037892f0022
Partho P. Das <parthopdas@live.com>
2019-08-18 16:19:00 +1000

    Switching to eslint and other confit from gatsby-starter-typescript-plus

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,7 +2,6 @@ import { injectGlobal } from 'react-emotion'
 import normalize from './normalize'
 import globals from './globals'
 
-// tslint:disable-next-line:no-unused-expression
 injectGlobal`
   ${normalize}
   ${globals}
